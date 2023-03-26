8d2e2ba865fa71a9f5b3ddc32f58eef25bd4916e
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2023-03-25 20:33:28 -0700

    Add icons.

--- src/app/components/buttons/icon-buttons/icon-buttons.component.ts
+++ src/app/components/buttons/icon-buttons/icon-buttons.component.ts
@@ -5,11 +5,20 @@ import { Component, Input } from '@angular/core';
   templateUrl: './icon-buttons.component.html',
   styleUrls: ['./icon-buttons.component.scss']
 })
-
+// Ace: fa-regular fa-bullseye-arrow
+// Kill: fa-solid fa-fire-flame-curved
+// Assist: fa-solid fa-handshake-angle"
+// Block: fa-solid fa-shield"
+// Dig: fa-solid fa-shovel"
+// Zero (Over the Net): fa-solid fa-overline"
+// Error: fa-solid fa-circle-exclamation"
+// First Hit: fa-solid fa-circle-1"
+// Second Hit: fa-solid fa-circle-2"
+// Zero Hit: fa-solid fa-circle-0"
 
 export class IconButtonsComponent {
   @Input() iconSource: string;
+  @Input() iconName: string;
   @Input() label: string;
-
 
 }
