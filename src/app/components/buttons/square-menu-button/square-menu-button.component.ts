8a9529968cb40fb13d54475b04c7db66401ec4ad
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2023-10-26 22:24:00 -0400

    Add more buttons and icons.

--- src/app/components/buttons/square-menu-button/square-menu-button.component.ts
+++ src/app/components/buttons/square-menu-button/square-menu-button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-square-menu-button',
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./square-menu-button.component.scss']
 })
 export class SquareMenuButtonComponent {
-
+  @Input() iconType: string;
 }
