1b8267112351f05857e7b0571fe00773d7a4f9f0
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2023-11-04 14:49:59 -0400

    Finish progress buttons and routing. Need player setup page next.

--- src/app/components/buttons/progress-buttons/progress-buttons.component.ts
+++ src/app/components/buttons/progress-buttons/progress-buttons.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-progress-buttons',
@@ -7,9 +8,24 @@ import { Component, Input } from '@angular/core';
 })
 export class ProgressButtonsComponent {
   //string path for page to navigate to
-  @Input() path: string;
+  @Input() path: string[];
   //text to display in button
-  @Input() buttonText: string;
+  @Input() buttonText: string[];
   //Defaults to right facing arrows
-  @Input() iconType: string = "continue";
+  @Input() iconType: string[] = ["continue"];
+  //Can be up to three in the row, defaults to one
+  @Input() numOfButtons: number = 1;
+  //1 is bottom right, default
+  //0 is middle
+  //-1 is bottom left
+  @Input() positioning: number = 1;
+
+  constructor(public router: Router) {
+
+  }
+
+  routeToPage() {
+    this.router.navigate(['/', this.path[0]]);
+  }
+
 }
