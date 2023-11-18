a45ab147d487f43750f4d0902f8b5c64d82d2947
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2023-11-18 11:21:06 -0500

    Add delay on animations.

--- src/app/player-select/player-select-tile/player-select-tile.component.ts
+++ src/app/player-select/player-select-tile/player-select-tile.component.ts
@@ -1,16 +1,31 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
 
 @Component({
   selector: 'app-player-select-tile',
   templateUrl: './player-select-tile.component.html',
   styleUrls: ['./player-select-tile.component.scss']
 })
-export class PlayerSelectTileComponent {
+export class PlayerSelectTileComponent implements OnChanges {
   @Input() name: string = "";
   @Input() username: string = "";
   @Input() profilePictureId: string; //idk how to implement this yet.
   @Input() color: string = "#F6F6F6";
-  @Input() selected: boolean = false; 
+  @Input() selected: boolean = false;
+  @Input() expanded: boolean = false;
+
+  expandDelay: boolean = this.expanded;
+  ngOnChanges(changes: SimpleChanges) {
+    if (changes['expanded']) {
+      if (changes['expanded'].currentValue) {
+        setTimeout(() => {
+          this.expandDelay = changes['expanded'].currentValue;
+        }, 500);
+      } else {
+        this.expandDelay = changes['expanded'].currentValue;
+      }
+    }
+
+  }
 
   getInitials() {
     const fullName = this.name.split(' ');
