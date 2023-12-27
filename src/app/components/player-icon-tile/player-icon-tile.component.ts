64908419ec45ad85f8e96f42439f22d0e7bc10c1
Munseok Kim <59575386+PresideMoon@users.noreply.github.com>
2023-12-26 21:21:13 -0500

    Fail to figure out spacing.

--- src/app/components/player-icon-tile/player-icon-tile.component.ts
+++ src/app/components/player-icon-tile/player-icon-tile.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-player-icon-tile',
@@ -6,5 +6,8 @@ import { Component } from '@angular/core';
   styleUrls: ['./player-icon-tile.component.scss']
 })
 export class PlayerIconTileComponent {
-
+  @Input() name: string = "";
+  @Input() profilePictureId: string; //idk how to implement this yet.
+  @Input() icon: string = "check";
+  @Input() color?: string = "#F6F6F6";
 }
